61d2bdebceb9b2a7ff269bcdc0829759c84d5e8f
lee zhuo hong <21018193@myrp.edu.sg>
2022-07-13 10:14:53 +0800

    Commit

--- app/src/main/java/sg/edu/rp/c346/id21018193/demodatabasecrud/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/demodatabasecrud/MainActivity.java
@@ -3,12 +3,61 @@ package sg.edu.rp.c346.id21018193.demodatabasecrud;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnAdd, btnEdit, btnRetrieve;
+    TextView tvDBContent;
+    EditText etContent;
+    ArrayList<Note> al;
+    ListView lv;
+    ArrayAdapter<Note> aa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize the variables with UI here
+
+        al = new ArrayList<Note>();
+        aa = new ArrayAdapter<Note>(this,
+                android.R.layout.simple_list_item_1, al);
+        lv.setAdapter(aa);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = etContent.getText().toString();
+                DBHelper dbh = new DBHelper(MainActivity.this);
+                long inserted_id = dbh.insertNote(data);
+
+                if (inserted_id != -1){
+                    al.clear();
+                    al.addAll(dbh.getAllNotes());
+                    aa.notifyDataSetChanged();
+                    Toast.makeText(MainActivity.this, "Insert successful",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnRetrieve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(MainActivity.this);
+                al.clear();
+                al.addAll(dbh.getAllNotes());
+                aa.notifyDataSetChanged();
+            }
+        });
     }
+
 }

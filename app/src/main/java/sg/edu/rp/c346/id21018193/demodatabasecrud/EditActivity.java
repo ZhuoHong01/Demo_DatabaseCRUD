82743c7ec353e8e0480638d4d351342e990c69b5
lee zhuo hong <21018193@myrp.edu.sg>
2022-07-13 10:23:51 +0800

    Commit

--- app/src/main/java/sg/edu/rp/c346/id21018193/demodatabasecrud/EditActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/demodatabasecrud/EditActivity.java
@@ -2,13 +2,34 @@ package sg.edu.rp.c346.id21018193.demodatabasecrud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EditActivity extends AppCompatActivity {
+
+    TextView tvID;
+    EditText etContent;
+    Button btnUpdate, btnDelete;
+    Note data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        //initialize the variables with UI here
+
+        Intent i = getIntent();
+        data = (Note) i.getSerializableExtra("data");
+
+        tvID.setText("ID: " + data.getId());
+        etContent.setText(data.getNoteContent());
+
+
+
     }
+
 }

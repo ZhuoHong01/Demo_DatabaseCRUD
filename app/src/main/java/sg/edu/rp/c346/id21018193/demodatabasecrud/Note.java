755c16146e6aa11ac36afdb84dffbf7c6e9d03a6
lee zhuo hong <21018193@myrp.edu.sg>
2022-07-13 10:02:49 +0800

    Commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21018193/demodatabasecrud/Note.java
@@ -0,0 +1,25 @@
+package sg.edu.rp.c346.id21018193.demodatabasecrud;
+
+import androidx.annotation.NonNull;
+
+public class Note {
+    private int id;
+    private String noteContent;
+
+    public Note(int id, String noteContent) {
+        this.id = id;
+        this.noteContent = noteContent;
+    }
+
+    public int getId() { return id; }
+
+    public String getNoteContent() { return noteContent; }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return id + "\n" + noteContent;
+    }
+}
+
+
